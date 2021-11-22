d75c03646d3128789ee4e5b084a7625363020400
AxelBlackXZ07 <92955733+AxelBlackXZ07@users.noreply.github.com>
2021-11-22 19:52:42 -0300

    Add files via upload

---
+++ src/otak.js
@@ -0,0 +1,18 @@
+const otak = (prefix, pushname) => {
+    return `*Palabras especificas para que el bot interactue con ustedes mis queridos virgotakus*
+
+
+Quien es tu sempai botsito
+Gimeme puta 
+Te amo botsito uwu
+Onichan
+La manosea 7w7
+
+
+_*Ojito escribe tal como esta*_
+
+ву Axelin`
+
+}
+
+exports.otak = otak
---
+++ src/version.js
@@ -0,0 +1,31 @@
+const version = (prefix, pushname) => {
+    return `
+*blackвσт ву axelin*
+
+*Actualizado:* 21 de octubre del 2021
+*Versión actual:* 4.0
+*Ofrecida por:* AxelBlack™
+
+
+*INFORME*
+
+Si no funciona el comando *play o *play2 checa el blog del bot y mira la version que poses 
+Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
+Cambiare la apikey pasando un mes o si veo muchos atercados o robos
+Si vez a alguien que robo mi bot comunicate conmigo 
+
+
+
+
+
+*SIGUEME EN INSTAGRAM 🥸🧉*
+
+https://www.instagram.com/pollitoo_kun/
+
+
+_*by Axelin*_
+`
+
+}
+
+exports.version = version
